8554c89c602b426c3273b958a4a04e08ff9574ba
Matthew Salvatore Giancola <matthew.giancola42@gmail.com>
2024-07-16 10:41:12 -0400

    Load function requiures a http.FileSystem

--- veni.go
+++ veni.go
@@ -4,7 +4,6 @@ import(
 	"net/http"
 )
 
-func Load(handler http.Handler) http.Handler{
-	//right now do nothing
-	return handler;
+func Load(fs http.FileSystem) http.Handler{
+	return http.FileServer(fs)
 }
